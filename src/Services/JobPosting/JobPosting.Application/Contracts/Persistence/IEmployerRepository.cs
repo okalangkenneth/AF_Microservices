1829460468503da517cc486b4176c8a87e4dbe3a
Kenneth Okalang <okalang.ok@gmail.com>
2023-02-20 10:38:09 +0100

    Developed  contracts in application

---
+++ src/Services/JobPosting/JobPosting.Application/Contracts/Persistence/IEmployerRepository.cs
@@ -0,0 +1,11 @@
+﻿using JobPosting.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JobPosting.Application.Contracts.Persistence
+{
+    public interface IEmployerRepository : IAsyncRepository<Job_Posting>
+    {
+        Task<IEnumerable<Job_Posting>> GetEmployersByUserName(string userName);
+    }
+}
---
+++ src/Services/JobPosting/JobPosting.Domain/Entities/Job_Posting.cs
@@ -0,0 +1,16 @@
+﻿using JobPosting.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPosting.Domain.Entities
+{
+    public class Job_Posting : EntityBase
+    {
+        public string JobCategory { get; set;}
+        public string JobCategoryDescription { get; set; }
+        public ICollection<Job_Posting> JobPostings { get; set; }
+    }
+}
